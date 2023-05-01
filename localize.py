2d6905508e646ab556f3f88b8f94448a3d32df8f
QinGuodong1287 <qinguodong07@qq.com>
2023-05-01 09:26:09 +0800

    Removed data files.

---
+++ localize.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+import constants
+
+lang_path = os.path.join(constants.program_path, r"lang")
+if not os.path.exists(lang_path):
+    need_mkdir = True
+elif not os.path.isdir(lang_path):
+    os.unlink(lang_path)
+    need_mkdir = True
+else:
+    need_mkdir = False
+if need_mkdir:
+    os.mkdir(lang_path)
+del need_mkdir
