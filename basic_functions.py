2ca9852887ad97a22c02b605b7bf1d9b56f23990
QinGuodong1287 <qinguodong07@qq.com>
2023-05-21 09:15:05 +0800

    Fix basic functions.

--- basic_functions.py
+++ basic_functions.py
@@ -17,24 +17,24 @@ def str_ljust(s: str, length: int) -> str:
     return ''.join((s, ' ' * (length - uni_len(s))))
 
 
-def loadFile(file_name, default=None):
-    if not os.path.exists(file_name):
+def loadFile(file_path, default=None):
+    if not os.path.exists(file_path):
         if default is None:
             return None
-        with open(file_name, "w", encoding="utf-8") as fp:
-            json.dump(default, fp, ensure_ascii=False)
-    with open(file_name, "r", encoding="utf-8") as fp:
+        with open(file_path, "w", encoding="utf-8") as fp:
+            json.dump(default, fp, indent=4, ensure_ascii=False)
+    with open(file_path, "r", encoding="utf-8") as fp:
         data = json.load(fp)
     return data
 
 
-def saveFile(file_name, data):
-    with open(file_name, "w", encoding="utf-8") as fp:
+def saveFile(file_path, data):
+    with open(file_path, "w", encoding="utf-8") as fp:
         json.dump(data, fp, indent=4, ensure_ascii=False)
 
 
-def loadDict(file_name, d):
-    new_d = loadFile(file_name, d)
+def loadDict(file_path, d):
+    new_d = loadFile(file_path, d)
     for key, value in new_d.items():
         d[key] = value
     return d
