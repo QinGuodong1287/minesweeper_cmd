8e41d239eb3b94da2495ca097892959218b614f8
QinGuodong1287 <qinguodong07@qq.com>
2023-05-21 09:43:33 +0800

    Add a space in restart message.

--- minesweeper.py
+++ minesweeper.py
@@ -20,7 +20,7 @@ def main() -> int:
             if retcode:
                 """print("抱歉，该游戏出现错误，可输入“python3 {}”并按Enter键重新启动游戏。".format(
                     sys.argv[0]))"""
-                choice = (input(localize.tr("抱歉，该游戏出现错误。是否重新启动游戏？(y/n)"))
+                choice = (input(localize.tr("抱歉，该游戏出现错误。是否重新启动游戏？(y/n) "))
                           .lower())
                 if not (choice and choice[0] == 'y'):
                     break
