a8efd7622bc9474402111de9b8cee3bcbeace6d5
QinGuodong1287 <qinguodong07@qq.com>
2023-05-21 09:00:16 +0800

    Fix localize.py.

--- localize.py
+++ localize.py
@@ -74,7 +74,7 @@ def tr(source_text: str) -> str:
         if localize_settings["log_untranslated_text_flag"]:
             global untranslated_text_list
             text_data = {
-                "text": source_text,
+                "text": repr(source_text),
                 "lang": localize_settings["lang"]}
             if text_data not in untranslated_text_list:
                 untranslated_text_list.append(text_data)
